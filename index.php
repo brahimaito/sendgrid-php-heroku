0f7039180febe3528134d6ad1ea0e6def7a0eb2c
Brahim Aito <brahimaito@gmail.com>
2020-06-19 17:12:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
     require 'vendor/autoload.php';
 
-    $from = new SendGrid\Email("Example User", getenv('FROM_EMAIL'));
+    $from = new SendGrid\Email("Example User", getenv('brahimaito@gmail.com'));
     $subject = "Hello from sendgrid-php";
-    $to = new SendGrid\Email("Example User", getenv('TO_EMAIL'));
+    $to = new SendGrid\Email("Example User", getenv('zizorify@gmail.com'));
     $content = new SendGrid\Content("text/plain", "Hello from sendgrid-php");
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
     
-    $apiKey = getenv('SENDGRID_API_KEY');
+    $apiKey = getenv('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');
     $sg = new \SendGrid($apiKey);
     
     $response = $sg->client->mail()->send()->post($mail);
